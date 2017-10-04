7d072d16ddf6adbcbb358c22ce6130e2dea18b17
SvetozarMateev <vidic9898@gmail.com>
2017-10-04 14:07:03 +0300

    added engine class

---
+++ just_football.js
@@ -0,0 +1,16 @@
+$(function () {
+    //TODO Implement Events
+    $('#fav-team').click(function () {
+
+    });
+    $('#first-league').click(function () {
+
+    });
+    $('#second-league').click(function () {
+
+    });
+    $('#third-league').click(function () {
+
+    });
+
+});
