84bebad8bd3de94e774bae7618ded23e7915f001
SvetozarMateev <vidic9898@gmail.com>
2017-10-04 12:36:53 +0300

    added basic Bootstrap navbar with no additional css

--- ajax_provider.js
+++ ajax_provider.js
@@ -3,54 +3,55 @@ $(function () {
     function getData(url) {
         console.log(3);
 
-            let allData;
+        let allData;
 
-            function setData(receivedData) {
-                allData = receivedData;
-                console.log(5);
+        function setData(receivedData) {
+            allData = receivedData;
+            console.log(5);
 
-            }
+        }
+
         console.log(4);
 
-            $.ajax({
-                url: url,
-                type: 'GET',
-                headers: {'X-Auth-Token': 'c6792b4e19a94f48b560ef221aebb6d0'},
-                success: (data => setData(data))
+        $.ajax({
+            url: url,
+            type: 'GET',
+            headers: {'X-Auth-Token': 'c6792b4e19a94f48b560ef221aebb6d0'},
+            success: (data => setData(data))
 
-            });
+        });
         console.log(6);
 
         return allData;
     }
- console.log(1);
+
+    console.log(1);
     const competitionsUrl = 'http://api.football-data.org/v1/competitions';
-    const teamsUrl='http://api.football-data.org/v1/';
+    const teamsUrl = 'http://api.football-data.org/v1/';
     console.log(2);
 
     const allCompetitions = getData(competitionsUrl);
 
 
     function getLeagueTable(leagueName) {
-     let currLeague = allCompetitions.filter(x => x.league === leagueName);
-     console.log(currLeague.leagueTable.href);
-     return getData(currLeague.leagueTable.href);
-     }
+        let currLeague = allCompetitions.filter(x => x.league === leagueName);
+        console.log(currLeague.leagueTable.href);
+        return getData(currLeague.leagueTable.href);
+    }
 
-     function getTeamsFromCompetition(leagueName) {
-     let currLeague = allCompetitions.filter(x => x.league === leagueName);
-     return getData(currLeague.teams.href);
-     }
+    function getTeamsFromCompetition(leagueName) {
+        let currLeague = allCompetitions.filter(x => x.league === leagueName);
+        return getData(currLeague.teams.href);
+    }
 
-     function getFixturesFromCompetition(leagueName) {
-     let currLeague = allCompetitions.filter(x => x.league === leagueName);
-     return getData(currLeague.fixtures.href);
-     }
+    function getFixturesFromCompetition(leagueName) {
+        let currLeague = allCompetitions.filter(x => x.league === leagueName);
+        return getData(currLeague.fixtures.href);
+    }
 
-     function getAllTeams(competitionId) {
-
-     return getData(competitionsUrl+'/'+competitionId+'/teams');
-     }
+    function getAllTeams(competitionId) {
+        return getData(competitionsUrl + '/' + competitionId + '/teams');
+    }
 
 
 });
