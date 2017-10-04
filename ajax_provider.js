917d6ec97ef015ba27b39dee238cd8230b76ed3d
SvetozarMateev <vidic9898@gmail.com>
2017-10-05 02:31:24 +0300

    fixed ajax_provider.js but more methods needed

--- ajax_provider.js
+++ ajax_provider.js
@@ -1,59 +1,111 @@
 $(function () {
+    const competitionsUrl = 'http://api.football-data.org/v1/competitions';
 
-    function getData(url) {
-        console.log(3);
 
-        let allData;
+    function visualizeLeagueLogos(callback, leagueName) {
 
-        function setData(receivedData) {
-            allData = receivedData;
-            console.log(5);
-
+        function firstPromise() {
+            let getCompetitions = new Promise((resolve, reject) => {
+                $.ajax({
+                    url: competitionsUrl,
+                    type: 'GET',
+                    headers: {'X-Auth-Token': 'c6792b4e19a94f48b560ef221aebb6d0'},
+                    success: (data => resolve(data))
+                });
+            });
+            return getCompetitions.then(filterByName);
         }
 
-        console.log(4);
 
-        $.ajax({
-            url: url,
-            type: 'GET',
-            headers: {'X-Auth-Token': 'c6792b4e19a94f48b560ef221aebb6d0'},
-            success: (data => setData(data))
+        function filterByName(data) {
+            let teamsLink;
 
-        });
-        console.log(6);
+            for (let competition of data) {
 
-        return allData;
+                if (competition.league === leagueName) {
+                    teamsLink = competition._links.teams.href;
+                    console.log(teamsLink);
+                }
+
+            }
+            return teamsLink;
+        }
+
+        function getLogos(data) {
+            let arr = [];
+            for (let club of data.teams) {
+                arr.push(club.crestUrl);
+            }
+            return arr;
+        }
+
+
+        function secondPromise(teamsLink) {
+            let getTeams = new Promise((resolve, reject) => {
+                $.ajax({
+                    url: teamsLink,
+                    type: 'GET',
+                    headers: {'X-Auth-Token': 'c6792b4e19a94f48b560ef221aebb6d0'},
+                    success: (data => resolve(data))
+                });
+            });
+             getTeams.then(getLogos).then(callback);
+        }
+        firstPromise().then(secondPromise);
     }
 
-    console.log(1);
-    const competitionsUrl = 'http://api.football-data.org/v1/competitions';
-    const teamsUrl = 'http://api.football-data.org/v1/';
-    console.log(2);
+    function visualizeLeagueTeam(callback,teamName,leagueName) {
+        function firstPromise() {
+            let getCompetitions = new Promise((resolve, reject) => {
+                $.ajax({
+                    url: competitionsUrl,
+                    type: 'GET',
+                    headers: {'X-Auth-Token': 'c6792b4e19a94f48b560ef221aebb6d0'},
+                    success: (data => resolve(data))
+                });
+            });
+            return getCompetitions.then(filterByName);
+        }
 
-    const allCompetitions = getData(competitionsUrl);
+
+        function filterByName(data) {
 
 
-    function getLeagueTable(leagueName) {
-        let currLeague = allCompetitions.filter(x => x.league === leagueName);
-        console.log(currLeague.leagueTable.href);
-        return getData(currLeague.leagueTable.href);
+            for (let competition of data) {
+
+                if (competition.league === leagueName) {
+                    return competition._links.teams.href;
+                }
+
+            }
+        }
+
+        function getTeam(data) {
+
+            for (let club of data.teams ) {
+                if(club.code===teamName){
+                    return club;
+                }
+            }
+        }
+
+
+        function secondPromise(teamsLink) {
+            let getTeams = new Promise((resolve, reject) => {
+                $.ajax({
+                    url: teamsLink,
+                    type: 'GET',
+                    headers: {'X-Auth-Token': 'c6792b4e19a94f48b560ef221aebb6d0'},
+                    success: (data => resolve(data))
+                });
+            });
+            getTeams.then(getTeam).then(callback);
+        }
+        firstPromise().then(secondPromise);
     }
 
-    function getTeamsFromCompetition(leagueName) {
-        let currLeague = allCompetitions.filter(x => x.league === leagueName);
-        return getData(currLeague.teams.href);
-    }
-
-    function getFixturesFromCompetition(leagueName) {
-        let currLeague = allCompetitions.filter(x => x.league === leagueName);
-        return getData(currLeague.fixtures.href);
-    }
-
-    function getAllTeams(competitionId) {
-        return getData(competitionsUrl + '/' + competitionId + '/teams');
-    }
-
-
+    //visualizeLeagueLogos(console.log, 'PL');
+    //visualizeLeagueTeam(console.log,'MUFC','PL');
 });
 
 
